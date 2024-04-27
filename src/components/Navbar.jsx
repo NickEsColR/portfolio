14f2672c00b7c475a0f908b45a6cb5983f15d373
NickEsColR <nicoesol0818@gmail.com>
2024-04-27 13:41:33 -0500

    feat: add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 import * as S from "./Navbar.styles";
 
 export const Navbar = () => {
-    return <S.NavbarContainer></S.NavbarContainer>;
+    return (<S.NavbarContainer>
+        <S.NavbarLogo>
+        <S.ImageLogo src="/favicon.ico" alt="Logo" />
+        <div>
+            <h1>Nicolas Colmenares</h1>
+            <h2>Frontend Developer</h2>
+        </div>
+        </S.NavbarLogo>
+    </S.NavbarContainer>);
 };
--- src/components/Navbar.styles.js
+++ src/components/Navbar.styles.js
@@ -1,5 +1,40 @@
 import styled from "styled-components";
+import { colors } from "../styles";
 
 export const NavbarContainer = styled.div`
+    max-width: 100vw;
+    height: 100px;
+    box-shadow: ${colors.BOX_SHADOW};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    background: ${colors.TEXT_WHITE};
+`;
 
+export const NavbarLogo = styled.div`
+    display: flex;
+    align-items: center;
+
+    h1,h2{
+        font-weight: 700,
+        color: ${colors.TEXT_BLACK};
+        margin:0;
+    }
+
+    h1{
+        font-size: 2rem;
+    }
+
+    h2{
+        background: ${colors.TEXT_GRADIENT_BLUE };
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+    }
+`;
+
+export const ImageLogo = styled.img`
+    width: 90px;
+    padding-right: 20px;
 `;
