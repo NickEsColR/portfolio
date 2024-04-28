d233a206368f5348184535f909e00d3d8ff549b6
NickEsColR <nicoesol0818@gmail.com>
2024-04-28 13:40:04 -0500

    feat:add responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import * as S from "./Navbar.styles";
 
 export const Navbar = () => {
-    return (<S.NavbarContainer>
-        <S.NavbarLogo>
-        <S.ImageLogo src="/favicon.ico" alt="Logo" />
-        <div>
-            <h1>Nicolas Colmenares</h1>
-            <h2>Frontend Developer</h2>
-        </div>
-        </S.NavbarLogo>
-    </S.NavbarContainer>);
+    const [showMenuBurger, setShowMenuBurger] = useState(false);
+
+    console.log(showMenuBurger);
+    return (
+        <S.NavbarContainer>
+            <S.NavbarLogo>
+                <S.ImageLogo src="/favicon.ico" alt="Logo" />
+                <div>
+                    <h1>Nicolas Colmenares</h1>
+                    <h2>Frontend Developer</h2>
+                </div>
+            </S.NavbarLogo>
+            <S.ContainerBurger onClick={()=>setShowMenuBurger(!showMenuBurger)}>
+                <S.BurgerMenu />
+                <S.BurgerMenu />
+                <S.BurgerMenu />
+            </S.ContainerBurger>
+            <S.ContainerItems $showMenuBurger={showMenuBurger}>
+                <p>About me</p>
+                <p>Projects</p>
+                <S.Button>Contact me</S.Button>
+            </S.ContainerItems>
+        </S.NavbarContainer>
+    );
 };
--- src/components/Navbar.styles.js
+++ src/components/Navbar.styles.js
@@ -9,6 +9,7 @@ export const NavbarContainer = styled.div`
     align-items: center;
     justify-content: space-between;
     padding: 0 20px;
+    margin:0;
     background: ${colors.TEXT_WHITE};
 `;
 
@@ -27,7 +28,7 @@ export const NavbarLogo = styled.div`
     }
 
     h2{
-        background: ${colors.TEXT_GRADIENT_BLUE };
+        background: ${colors.TEXT_GRADIENT_BLUE};
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
         background-clip: text;
@@ -37,4 +38,72 @@ export const NavbarLogo = styled.div`
 export const ImageLogo = styled.img`
     width: 90px;
     padding-right: 20px;
+`;
+
+export const ContainerItems = styled.div`
+    display: flex;
+    align-items: center;
+
+    p {
+        cursor: pointer;
+        padding: 0 13px;
+        text-decoration: none;
+        color: ${colors.TEXT_BLACK};
+        font-weight: 500;
+    }
+
+    @media (max-width: 768px) {
+        display: ${(props) => (props.$showMenuBurger ? "inline-block" : "none")};
+        position: absolute;
+        top: 100px;
+        background: ${colors.TEXT_WHITE};
+        box-shadow: ${colors.BOX_SHADOW};
+        padding: 10px 0 20px 0;
+        width: 100%;
+        left: 0;
+
+        a {
+            padding: 10px 20px;
+            display: block;
+            text-align: center;
+            color: ${colors.TEXT_BLACK};
+            font-weight: 500;
+        }
+    }
+`;
+
+export const Button = styled.button`
+    color: ${colors.TEXT_WHITE};
+    box-shadow: ${colors.BOX_SHADOW_BUTTON};
+    background: ${colors.TEXT_GRADIENT_BLUE};
+    padding: 10px 20px;
+    border-radius: 999px;
+    border: none;
+    margin: 10px 0;
+    font-size: 16px;
+    font-weight: 700;
+    cursor: pointer;
+
+    &:hover {
+        background: ${colors.TEXT_GRADIENT_BLUE_HOVER};
+    }
+
+    @media (max-width: 768px) {
+        width: 50%;
+        margin: 5px 10px 0 10px;
+    }
+`;
+
+export const ContainerBurger = styled.div`
+    display: none;
+    @media (max-width: 768px) {
+        display: block;
+    }
+`;
+
+export const BurgerMenu = styled.div`
+    width: 35px;
+    height: 2px;
+    background: ${colors.TEXT_BLACK};
+    margin: 6px 0;
 `;
