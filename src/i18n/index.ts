79d8decfb9b7c1c54021fb938974484df7379f0b
NickEsColR <nicoesol0818@gmail.com>
2025-02-25 18:32:26 +0000

    feat: add frequent questions

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -9,3 +9,63 @@ export const getI18NCV = ({
   if (currentLocale === "es") return spanishCV;
   return englishCV;
 };
+
+export const getI18ChatQuestions = ({
+  currentLocale,
+}: {
+  currentLocale: string | undefined;
+}) => {
+  if (currentLocale === "es")
+    return [
+      {
+        min: "¿Quién eres?",
+        complete: "¿Quien eres?",
+      },
+      {
+        min: "Experiencia",
+        complete: "Háblame de tu experiencia",
+      },
+      {
+        min: "Proyectos",
+        complete: "Háblame de tus proyectos",
+      },
+      {
+        min: "Educación",
+        complete: "Háblame de tu educación",
+      },
+      {
+        min: "Habilidades",
+        complete: "Háblame de tus habilidades",
+      },
+      {
+        min: "Idiomas",
+        complete: "Háblame que idiomas hablas",
+      },
+    ];
+  return [
+    {
+      min: "Who are you?",
+      complete: "Who are you?",
+    },
+    {
+      min: "Experience",
+      complete: "Tell me about your experience",
+    },
+    {
+      min: "Projects",
+      complete: "Tell me about your projects",
+    },
+    {
+      min: "Education",
+      complete: "Tell me about your education",
+    },
+    {
+      min: "Skills",
+      complete: "Tell me about your skills",
+    },
+    {
+      min: "Languages",
+      complete: "Tell me about languages you speak",
+    },
+  ];
+};
